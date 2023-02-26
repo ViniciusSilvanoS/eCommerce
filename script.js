3123661663f55011790a5741e3464066a7614a4f
Bruno-Pasqual <brunoh.pasqual0@gmail.com>
2023-02-26 01:57:53 -0300

    JSON ok

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-//! quem ler é corno
+const puxaDados = async () => {
+  try {
+    const response = await fetch('./data.JSON');
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+puxaDados();
